20297c1f88c549888184c8a6fdf17128f802fbce
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-11-22 22:30:36 -0500

    All the channel logic

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/labstack/echo/v5"
 	expo "github.com/oliveroneill/exponent-server-sdk-golang/sdk"
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
@@ -55,46 +57,20 @@ func main() {
 	})
 
 	// Announcements notifications
-	app.OnRecordAfterCreateRequest().Add(func(e *core.RecordCreateEvent) error {
-		if e.Collection.Name == "announcements" {
-			location := e.Record.GetString("location")
+	app.OnRecordAfterCreateRequest("announcements").Add(func(e *core.RecordCreateEvent) error {
+		location := e.Record.GetString("location")
 
-			if location == "" {
-				devices, err := app.Dao().FindRecordsByFilter("devices", "token ~ \"ExponentPushToken\"", "-created", 0, 0, nil)
+		if location == "" {
+			devices, err := app.Dao().FindRecordsByFilter("devices", "token ~ \"ExponentPushToken\"", "-created", 0, 0, nil)
 
-				if err != nil {
-					log.Default().Panicln(err)
-				}
-
-				tokens := []string{}
-
-				for _, v := range devices {
-					tokens = append(tokens, v.GetString("token"))
-				}
-
-				for _, t := range tokens {
-					announce(app, e.Record, t)
-				}
-			}
-
-			users, err := app.Dao().FindRecordsByFilter("users", "location = {:location}", "-created", 0, 0, dbx.Params{"location": location})
-		
 			if err != nil {
-				log.Default().Println(err)
+				log.Default().Panicln(err)
 			}
 
 			tokens := []string{}
 
-			for _, v := range users {
-				app.Dao().ExpandRecord(v, []string{"devices"}, nil)
-
-				devices := v.Expand()["devices"]
-
-				if devices != nil {
-					for _, v := range devices.([]*models.Record) {
-						tokens = append(tokens, v.GetString("token"))
-					}
-				}
+			for _, v := range devices {
+				tokens = append(tokens, v.GetString("token"))
 			}
 
 			for _, t := range tokens {
@@ -102,65 +78,225 @@ func main() {
 			}
 		}
 
-		return nil
-	})
+		users, err := app.Dao().FindRecordsByFilter("users", "location = {:location}", "-created", 0, 0, dbx.Params{"location": location})
+	
+		if err != nil {
+			log.Default().Println(err)
+		}
 
-	app.OnRecordAfterCreateRequest().Add(func (e *core.RecordCreateEvent) error {
-		if e.Collection.Name == "messages" {
-			app.Dao().ExpandRecord(e.Record, []string{"channel"}, nil)
-			channel := e.Record.Expand()["channel"].(*models.Record)
-			app.Dao().ExpandRecord(channel, []string{"users"}, nil)
-			users := channel.Expand()["users"].([]*models.Record)
-			
-			tokens := []string{}
+		tokens := []string{}
 
-			for _, u := range users {
-				if u.Id == e.Record.GetString("user") {
-					continue
-				}
+		for _, v := range users {
+			app.Dao().ExpandRecord(v, []string{"devices"}, nil)
 
-				app.Dao().ExpandRecord(u, []string{"devices"}, nil)
-				devices := u.Expand()["devices"]
+			devices := v.Expand()["devices"]
 
-				if devices != nil {
-					for _, d := range devices.([]*models.Record) {
-						tokens = append(tokens, d.GetString("token"))
-					}
-				}
-			}
-
-			client := expo.NewPushClient(nil)
-
-			for _, t := range tokens {
-				pushToken, err := expo.NewExponentPushToken(t)
-			
-				if err != nil {
-					log.Default().Panicln(err)
-				}
-			
-				app.Dao().ExpandRecord(e.Record, []string{"user"}, nil)
-				user := e.Record.Expand()["user"].(*models.Record)
-				user_name := user.GetString("name")
-				title := fmt.Sprint("New announcement from ", user_name)
-				body := e.Record.GetString("content")
-			
-				response, err := client.Publish(
-					&expo.PushMessage{
-						To: []expo.ExponentPushToken{pushToken},
-						Body: body,
-						Title: title,
-					},
-				)
-			
-				if err != nil {
-					log.Default().Panicln(err)
-				}
-			
-				if response.ValidateResponse() != nil {
-					fmt.Println(response.PushMessage.To, "failed")
+			if devices != nil {
+				for _, v := range devices.([]*models.Record) {
+					tokens = append(tokens, v.GetString("token"))
 				}
 			}
 		}
+
+		for _, t := range tokens {
+			announce(app, e.Record, t)
+		}
+
+		return nil
+	})
+
+	// Creating channels for announcements
+	app.OnRecordAfterCreateRequest("announcements").Add(func (e *core.RecordCreateEvent) error {
+		channels_collection, _ := app.Dao().FindCollectionByNameOrId("channels")
+		new_channel := models.NewRecord(channels_collection)
+		new_channel.Set("isDefault", false)
+
+		app.Dao().ExpandRecord(e.Record, []string{"location"}, nil)
+		location := e.Record.Expand()["location"].(*models.Record)
+
+		new_channel.Set("users", location.Get("leaders").([]string))
+		new_channel.Set("announcement", e.Record.Id)
+		app.Dao().SaveRecord(new_channel)
+
+		return nil
+	})
+
+	// Push notifications for messages
+	app.OnRecordAfterCreateRequest("messages").Add(func (e *core.RecordCreateEvent) error {
+		app.Dao().ExpandRecord(e.Record, []string{"channel"}, nil)
+		channel := e.Record.Expand()["channel"].(*models.Record)
+		app.Dao().ExpandRecord(channel, []string{"users"}, nil)
+		users := channel.Expand()["users"].([]*models.Record)
+		
+		tokens := []string{}
+
+		for _, u := range users {
+			if u.Id == e.Record.GetString("user") {
+				continue
+			}
+
+			app.Dao().ExpandRecord(u, []string{"devices"}, nil)
+			devices := u.Expand()["devices"]
+
+			if devices != nil {
+				for _, d := range devices.([]*models.Record) {
+					tokens = append(tokens, d.GetString("token"))
+				}
+			}
+		}
+
+		client := expo.NewPushClient(nil)
+
+		for _, t := range tokens {
+			pushToken, err := expo.NewExponentPushToken(t)
+		
+			if err != nil {
+				log.Default().Panicln(err)
+			}
+		
+			app.Dao().ExpandRecord(e.Record, []string{"user"}, nil)
+			user := e.Record.Expand()["user"].(*models.Record)
+			user_name := user.GetString("name")
+			title := fmt.Sprint("New announcement from ", user_name)
+			body := e.Record.GetString("content")
+		
+			response, err := client.Publish(
+				&expo.PushMessage{
+					To: []expo.ExponentPushToken{pushToken},
+					Body: body,
+					Title: title,
+				},
+			)
+		
+			if err != nil {
+				log.Default().Panicln(err)
+			}
+		
+			if response.ValidateResponse() != nil {
+				fmt.Println(response.PushMessage.To, "failed")
+			}
+		}
+
+		return nil
+	})
+
+	app.OnRecordAfterCreateRequest("locations").Add(func (e *core.RecordCreateEvent) error {
+		channels_collection, _ := app.Dao().FindCollectionByNameOrId("channels")
+		new_channel := models.NewRecord(channels_collection)
+		new_channel.Set("isDefault", true)
+		location_leaders := e.Record.Get("leaders").([]string)
+		new_channel.Set("users", location_leaders)
+		app.Dao().SaveRecord(new_channel)
+		return nil
+	})
+
+	// Location changing logic and channels
+	app.OnRecordBeforeUpdateRequest("users").Add(func (e *core.RecordUpdateEvent) error {
+		app.Dao().ExpandRecord(e.Record, []string{"location"}, nil)
+		location := e.Record.Expand()["location"].(*models.Record)
+		app.Dao().ExpandRecord(location, []string{"leaders"}, nil)
+		leaders := location.Expand()["leaders"].([](*models.Record))
+
+		is_leader := false
+		for _, element := range leaders {
+			if element.Id == e.Record.Id {
+				is_leader = true
+				break
+			}
+		}
+
+		old_location_id := e.Record.OriginalCopy().GetString("location")
+		new_location_id := e.Record.GetString("location")
+
+		location_changed := old_location_id != new_location_id
+
+		old_location, _ := app.Dao().FindRecordById("locations", old_location_id)
+		old_location_leaders := old_location.Get("leaders").([]string)
+		was_leader := false
+		for _, v := range old_location_leaders {
+			if v == e.Record.Id {
+				was_leader = true
+				break
+			}
+		}
+
+		if location_changed {
+			if was_leader {
+				// remove from old channels
+				old_channels, _ := app.Dao().FindRecordsByFilter("channels", "(users ?~ {:user})", "-created", 0, 0, dbx.Params{"user": e.Record.Id})
+				for _, v := range old_channels {
+					channel := v
+					users := channel.Get("users").([]string)
+					new_users := []string{}
+					for _, u := range users {
+						if u != e.Record.Id {
+							new_users = append(new_users, u)
+						}
+					}
+					channel.Set("users", new_users)
+					app.Dao().SaveRecord(channel)
+				}
+			} else {
+				// delete old channel
+				default_channel, _ := app.Dao().FindRecordsByFilter("channels", "(users ?~ {:user} && isDefault = True)", "-created", 0, 0, dbx.Params{"user": e.Record.Id})
+				if len(default_channel) > 0 {
+					channel := default_channel[0]
+					app.Dao().Delete(channel)
+
+				}
+			}
+			
+			if is_leader {
+				// add to new channels
+				new_location, _ := app.Dao().FindRecordById("locations", new_location_id)
+				new_location_leaders := new_location.Get("leaders").([]string)
+				leader := new_location_leaders[0]
+				new_channels, _ := app.Dao().FindRecordsByFilter("channels", "(users ?~ {:user})", "-created", 0, 0, dbx.Params{"user": leader})
+				
+				for _, v := range new_channels {
+					channel := v
+					users := channel.Get("users").([]string)
+					users = append(users, e.Record.Id)
+					channel.Set("users", users)
+					app.Dao().SaveRecord(channel)
+				}
+			} else {
+				// create new channel
+				channels_collection, _ := app.Dao().FindCollectionByNameOrId("channels")
+				channel := models.NewRecord(channels_collection)
+				users := []string{e.Record.Id}
+				leader_ids := []string{}
+				for _, v := range leaders {
+					if v.GetString("location") == new_location_id {
+						leader_ids = append(leader_ids, v.Id)
+					}
+				}
+				users = append(users, leader_ids...)
+				channel.Set("users", users)
+				channel.Set("isDefault", true)
+
+				app.Dao().SaveRecord(channel)
+			}
+		}
+
+		return nil
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.POST("/api/rsvp", func (c echo.Context) error {
+			announcement_id := c.QueryParam("announcement_id")
+			announcement_channels, _ := app.Dao().FindRecordsByFilter("channels", "announcement = {:announcement_id}", "-created", 0, 0, dbx.Params{"announcement_id": announcement_id})
+			announcement_channel := announcement_channels[0]
+
+			auth_record, _ := c.Get(apis.ContextAuthRecordKey).(*models.Record)
+
+			if auth_record == nil {
+				return c.JSON(http.StatusUnauthorized, map[string]interface{}{"status": "unauthorized"})
+			}
+			
+			announcement_channel.Set("users", append(announcement_channel.Get("users").([]string), auth_record.Id))
+			return c.JSON(http.StatusOK, map[string]interface{}{"status": "ok"})
+		})
 
 		return nil
 	})
